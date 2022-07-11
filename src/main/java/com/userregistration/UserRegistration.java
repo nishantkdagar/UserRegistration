bcf248472b61c800200209dfbfcbab43c975eda6
Nishan Kumar <nishantkumarkd@gmail.com>
2022-07-11 10:10:24 +0530

    Checking Email Validity

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -7,15 +7,18 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     public static void main(String[] args){
-        System.out.println("To Check Name Validity, Press 1 for First Name or 2 for Last Name");
+        System.out.println("To Check Name Validity, Press 1 for First Name or 2 for Last Name and 3 for Email");
         Scanner input = new Scanner(System.in);
         switch (input.nextInt()){
             case 1:
-            firstName();
-            break;
+                firstName();
+                break;
             case 2:
-            lastName();
-            break;
+                lastName();
+                break;
+            case 3:
+                emailID();
+                break;
             default:
                 System.out.println("Please Enter either 1 or 2");
         }
@@ -47,6 +50,20 @@ public class UserRegistration {
             System.out.println("Last name is Valid");
         }else{
             System.out.println("Invalid Last Name");
+        }
+    }
+    public static void emailID(){
+        String regex="^[a-z]{3}[a-zA-z.]{0,}+@+bl.co+[a-z.]{0,}$";
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Email Address: ");
+        String email = input.nextLine();
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        boolean result = matcher.matches();
+        if(result){
+            System.out.println("Email is valid");
+        }else {
+            System.out.println("Email is invalid");
         }
     }
 }
