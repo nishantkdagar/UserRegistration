f392377d100218af35b665bf045b2b2a96368ee7
Nishan Kumar <nishantkumarkd@gmail.com>
2022-07-11 10:02:00 +0530

    Added code to check Last Name

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -7,15 +7,24 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     public static void main(String[] args){
-        System.out.println("Check if First Name is Valid");
-        firstName();
+        System.out.println("To Check Name Validity, Press 1 for First Name or 2 for Last Name");
+        Scanner input = new Scanner(System.in);
+        switch (input.nextInt()){
+            case 1:
+            firstName();
+            break;
+            case 2:
+            lastName();
+            break;
+            default:
+                System.out.println("Please Enter either 1 or 2");
+        }
     }
     public static void  firstName(){
-        String name;
         String regex = "^[A-Z]{1}[a-z]{2,}$";
         Scanner input = new Scanner(System.in);
         System.out.print("Enter First Name: ");
-        name = input.nextLine();
+        String name = input.nextLine();
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(name);
         boolean result = matcher.matches();
@@ -24,6 +33,20 @@ public class UserRegistration {
             System.out.println("Valid First Name");
         }else {
             System.out.println("Invalid First Name");
+        }
+    }
+    public static void lastName(){
+        String regex="^[A-Z]{1}[a-z]{2,}$";
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Last Name: ");
+        String name = input.nextLine();
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(name);
+        boolean result = matcher.matches();
+        if (result){
+            System.out.println("Last name is Valid");
+        }else{
+            System.out.println("Invalid Last Name");
         }
     }
 }
